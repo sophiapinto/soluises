df0d034b1451503c8e889006817df12b2acced75
Sophia Pinto <sarahsophiapinto@hotmail.com>
2024-01-26 19:55:04 -0300

    🚧 chore: Update landing

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -172,7 +172,7 @@ function Landing() {
                                     rel='noreferrer'
                                 >
                                     <Button className={classes.resumeBtn}>
-                                        Download CV
+                                        Currículo
                                     </Button>
                                 </a>
                             )}
@@ -183,7 +183,7 @@ function Landing() {
                                 duration={2000}
                             >
                                 <Button className={classes.contactBtn}>
-                                    Contact
+                                    Contato
                                 </Button>
                             </NavLink>
                         </div>
