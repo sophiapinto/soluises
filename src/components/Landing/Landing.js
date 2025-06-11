40654f11de04ed6221044560bf630c5e30928107
sophiapinto <sarahsophiapinto@hotmail.com>
2025-06-11 13:25:56 -0300

    add assets

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -60,7 +60,7 @@ function Landing() {
                         className='lcr--content'
                         style={{ color: theme.tertiary }}
                     >
-                        <h6>{headerData.title}</h6>
+
                         <h1>{headerData.name}</h1>
                         <p>{headerData.desciption}</p>
 
