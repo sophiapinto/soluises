8ebd849131c8bc32ccbce048d516654efd3ab3be
sophiapinto <sarahsophiapinto@hotmail.com>
2025-06-19 18:54:08 -0300

    Remove Soluises Navbar estilizado

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -122,20 +122,9 @@ function Navbar() {
 
     const classes = useStyles();
 
-    const shortname = (name) => {
-        if (name.length > 12) {
-            return name.split(' ')[0];
-        } else {
-            return name;
-        }
-    };
-
     return (
         <div className='navbar'>
             <div className='navbar--container'>
-                <h1 style={{ color: theme.secondary }}>
-                    {shortname(headerData.name)}
-                </h1>
 
                 <IoMenuSharp
                     className={classes.navMenu}
