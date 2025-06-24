57fe55d47a49d83a1b1ae66d88755c1691820141
fredcrodrigues <fredsoncostaa@gmail.com>
2025-06-24 11:29:48 -0300

    Alterando componentes do report

--- src/components/Projects/SingleProject/SingleProject.js
+++ src/components/Projects/SingleProject/SingleProject.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import {FaDownload , FaEye  } from 'react-icons/fa';
 import Fade from 'react-reveal/Fade';
 
 import placeholder from '../../../assets/png/placeholder.png';
 import './SingleProject.css';
 
-function SingleProject({ id, name, desc, tags, image, theme }) {
+function SingleProject({ id, name, desc, tags, image, code, theme }) {
     const useStyles = makeStyles((t) => ({
         iconBtn: {
             display: 'flex',
@@ -34,14 +35,76 @@ function SingleProject({ id, name, desc, tags, image, theme }) {
     const classes = useStyles();
 
     return (
-        <Fade bottom>
+          <Fade bottom>
             <div
                 key={id}
                 className='singleProject'
-                style={{ backgroundColor: '#D63826', borderRadius: '20px'}}
+                 style={{ backgroundColor: '#D63826', borderRadius: '20px'}}
             >
                 <div className='projectContent'>
+                  
                     <img src={image ? image : placeholder} alt={name} />
+                    <div className='project--showcaseBtn'>
+                        <a
+                           
+                            target='_blank'
+                            download='ebook'
+                            rel='noreferrer'
+                            className={classes.iconBtn}
+                            aria-labelledby={`${name
+                                .replace(' ', '-')
+                                .toLowerCase()} ${name
+                                .replace(' ', '-')
+                                .toLowerCase()}-demo`}
+                        >
+                            <FaDownload
+                                id={`${name
+                                    .replace(' ', '-')
+                                    .toLowerCase()}-demo`}
+                                className={classes.icon}
+                                aria-label='Demo'
+                            />
+                        </a>
+                        <a
+                            href={code}
+                            target='_blank'
+                            rel='noreferrer'
+                            className={classes.iconBtn}
+                            aria-labelledby={`${name
+                                .replace(' ', '-')
+                                .toLowerCase()} ${name
+                                .replace(' ', '-')
+                                .toLowerCase()}-code`}
+                        >
+                            <FaEye
+                                id={`${name
+                                    .replace(' ', '-')
+                                    .toLowerCase()}-code`}
+                                className={classes.icon}
+                                aria-label='Code'
+                            />
+                        </a>
+                    </div>
+                </div>
+                <p
+                    className='project--desc'
+                    style={{
+                        background: theme.secondary,
+                        color: theme.tertiary,
+                    }}
+                >
+                    {desc}
+                </p>
+                <div
+                    className='project--lang'
+                    style={{
+                        background: theme.secondary,
+                        color: theme.tertiary,
+                    }}
+                >
+                    {tags.map((tag, id) => (
+                        <span key={id}>{tag}</span>
+                    ))}
                 </div>
             </div>
         </Fade>
